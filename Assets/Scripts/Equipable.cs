ea7a87af084adddcc58d9030f3c0d1e4651e0070
Andrés <nosoysaenz@gmail.com>
2021-05-14 20:47:52 -0600

    first

---
+++ Assets/Scripts/Equipable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public enum ItemType { Head, Torso }
+
+public class Equipable : MonoBehaviour {
+
+    public ItemType Type;
+    public Material material;
+
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    [SerializeField] CharacterCreator creator;
+    [SerializeField] Transform spawn;
+
+    GameObject c;
+
+    public void CreateCharacter() {
+        if (c != null)
+            Destroy(c);
+
+        c = creator.RandomCharacter(spawn);
+    }
+}
