529f49f0d47c77f53d8f2d28819a059919c06c98
alebertozzigo <47615243+alebertozzigo@users.noreply.github.com>
2021-05-23 11:57:11 -0600

    cambios

---
+++ Assets/Scripts/Game/TextoMuerte.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextoMuerte : MonoBehaviour
+{
+    [SerializeField] GameObject text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.SetActive(false);
+        StartCoroutine(Waiting(2f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //wait for 2 seconds
+    private IEnumerator Waiting(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        text.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Game/VictimaMuriendo1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictimaMuriendo1 : MonoBehaviour
+{
+    public Animator animator;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        StartCoroutine(Waiting(0.7f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+    }
+
+    //wait for 2 seconds
+    private IEnumerator Waiting(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        animator.SetBool("Muriendo", true);
+    }
+
+
+}
