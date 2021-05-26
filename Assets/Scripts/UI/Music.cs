41f64903d3d32d6b5f067d5b8fccba2ff947c5a6
Andrés <nosoysaenz@gmail.com>
2021-05-25 21:26:49 -0600

    trabajando en el master

--- Assets/Scripts/UI/Music.cs
+++ Assets/Scripts/UI/Music.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Music : MonoBehaviour {
+
+    public static Music music;
     private void Awake() {
-        DontDestroyOnLoad(gameObject);
+        if (music == null) {
+            DontDestroyOnLoad(gameObject);
+            music = this;
+        } else if (music != this) {
+            Destroy(gameObject);
+        }
     }
 }
