0ea66098db2ef586959ea97a77f6e7ddfbf14471
Andrés <nosoysaenz@gmail.com>
2021-05-26 21:23:02 -0600

    music

--- Assets/Scripts/UI/Music.cs
+++ Assets/Scripts/UI/Music.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Music : MonoBehaviour {
 
     public static Music music;
+    AudioSource source;
+
     private void Awake() {
         if (music == null) {
             DontDestroyOnLoad(gameObject);
             music = this;
         } else if (music != this) {
             Destroy(gameObject);
+        }
+    }
+
+    private void Start() {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void Update() {
+        if (SceneManager.GetActiveScene().name != "Game" && SceneManager.GetActiveScene().name != "Menu" && SceneManager.GetActiveScene().name != "HowTo" && SceneManager.GetActiveScene().name != "Story") {
+            source.volume = 0.03f;
+        } else {
+            source.volume = 0.1f;
         }
     }
 }
